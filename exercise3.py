a3e43feb109dd39d22762cd125e0e7f5e21c3437
sultana84 <sultanaisgneiss@gmail.com>
2016-07-28 23:19:34 +0000

    assignment 3 files

--- exercise3.py
+++ exercise3.py
@@ -1,13 +1,7 @@
-class MyRootModel(MySerializer):
-  my_attrs = {'an_attr': int, 'another_attr': str}
-  an_attr = ('I am an attr')
-  another_attr = (6)
-  a_third_attr = ('I should not exist')
-
-  def __init__(self, **kwargs):
+class MyRootModel(object):
+   my_attrs = {'an_attr': int, 'another_attr': str}
+   def __init__(self, **kwargs):
      for i, k in self.my_attrs.items:
-         print('Adding my attrs:{}'.format(i))
-         print('Adding my values:{}'.format(k))
          value_type = k(kwargs.get(i))
          setattr(self, i, value_type)
 
---
+++ my_dict.py
@@ -0,0 +1,2 @@
+my_dict = {'an_attr': 'I am an attr', 'another_attr': '6', 'a_third_attr':
+            'I should not exist'} 
